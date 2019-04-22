765ca26d3fe24d298fd01d40813bc50b10fbe404
Christy <christy.tomecek@yale.edu>
2019-04-22 10:05:13 -0400

    Update Get_Administrative_Documents_By_EAD_ID.sql

--- Get_Administrative_Documents_By_EAD_ID.sql
+++ Get_Administrative_Documents_By_EAD_ID.sql
@@ -2,7 +2,7 @@ SELECT resource.title, resource.ead_id, ao.title
 ,CONCAT('/repositories/14/archival_objects/', ao.id) AS ao_uri
 ,CONCAT('/repositories/14/resources/', resource.id) AS resource_uri
 FROM resource
-LEFT JOIN resource ON ao.root_record_id=resource.id
-WHERE ead_id IN () #add EAD IDs, e.g. 'mssa.hvt.0001'
+LEFT JOIN archival_object ao ON resource.id=ao.root_record_id
+WHERE resource.ead_id IN () #add EAD IDs, e.g. 'mssa.hvt.0001'
 AND ao.title='Administrative documents'
 ORDER BY resource.ead_id
